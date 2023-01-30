d3bcdde510b0a73c4755e1d79d2b13ce54020296
ze.su <ze.su@tongdun.net>
2023-01-30 11:01:06 +0800

    v2.0.0

--- index.js
+++ index.js
@@ -16,25 +16,24 @@ program
           name: 'template',
           type: 'list',
           message: '请选择一个模板',
-          choices: ['umi+dva', 'cra+mobx'],
+          choices: ['后台', '大屏'],
         },
       ])
       .then(({ template }) => {
         const writePath = path.join(process.cwd(), name);
         const readPath =
-          template === 'umi+dva'
-            ? 'direct:https://github.com/SsZzzz/umi-app'
-            : 'direct:https://github.com/SsZzzz/react-app';
+          template === '后台'
+            ? 'direct:https://github.com/SsZzzz/vite-project#basic'
+            : 'direct:https://github.com/SsZzzz/vite-project#map';
         const spinner = ora('正在下载模板, 请稍后...');
         spinner.start();
         download(readPath, writePath, { clone: true }, (error) => {
-          if (!error) {
-            // success download
-            spinner.succeed();
-            console.log(chalk.green('success! 项目初始化成功！'));
-          } else {
+          if (error) {
             spinner.fail();
             console.log(chalk.red('failed! 拉取模板失败', error));
+          } else {
+            spinner.succeed();
+            console.log(chalk.green('success! 项目初始化成功！'));
           }
         });
       });
